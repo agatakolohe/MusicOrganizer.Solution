a25ba0a03ecabf7b7b86138ef6a7929834b936a8
agatakolohe <agatakolohe@gmail.com>
2020-12-17 13:54:49 -0800

    get album list test, pass
    Co-authored-by: Glen Buck <glenbuck503@gmail.com>

--- MusicOrganizer.Tests/ModelTests/AlbumTests.cs
+++ MusicOrganizer.Tests/ModelTests/AlbumTests.cs
@@ -44,8 +44,6 @@ namespace MusicOrganizer.Tests
         {
             string albumName01 = "back in black";
             string albumName02 = "houses of holy";
-            string albumName03 = "houses of holy";
-            Album newAlbum03 = new Album(albumName03);
             Album newAlbum01 = new Album(albumName01);
             Album newAlbum02 = new Album(albumName02);
             List<Album> newList = new List<Album> { newAlbum01, newAlbum02 };
